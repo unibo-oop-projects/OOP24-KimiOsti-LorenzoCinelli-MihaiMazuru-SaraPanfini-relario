6f43cb660970c58f69ebebb7b441db91a213022a
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-14 17:00:10 +0100

    Added JavaDoc comment for Position Interface

--- src/main/java/it/unibo/oop/relario/utils/Position.java
+++ src/main/java/it/unibo/oop/relario/utils/Position.java
@@ -1,5 +1,9 @@
 package it.unibo.oop.relario.utils;
 
+/**
+ * Interface to handle positions throughout the game.
+ * @param T The type of the single coordinates.
+ */
 public interface Position<T> {
     public T getX();
     public T getY();
