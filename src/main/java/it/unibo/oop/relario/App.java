730a2391c99c791e91113ab434c32cc5da6ebb04
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-14 18:55:13 +0100

    Added JavaDoc comment for main class

--- src/main/java/it/unibo/oop/relario/App.java
+++ src/main/java/it/unibo/oop/relario/App.java
@@ -1,5 +1,8 @@
 package it.unibo.oop.relario;
 
+/**
+ * The main class of the application, which starts its execution.
+ */
 public class App {
     public static void main(String[] args) {
         
