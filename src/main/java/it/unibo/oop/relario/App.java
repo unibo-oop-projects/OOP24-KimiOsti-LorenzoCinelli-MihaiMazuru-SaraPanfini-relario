9756bfe301a8aa2ccb9ee5cddb99eb1a22ca881f
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-14 18:58:28 +0100

    Added JavaDoc comment for main method

--- src/main/java/it/unibo/oop/relario/App.java
+++ src/main/java/it/unibo/oop/relario/App.java
@@ -4,6 +4,10 @@ package it.unibo.oop.relario;
  * The main class of the application, which starts its execution.
  */
 public class App {
+    /**
+     * The main method of the application, from which execution starts.
+     * @param args are ignored.
+     */
     public static void main(String[] args) {
         
     }
