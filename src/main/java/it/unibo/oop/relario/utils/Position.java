d6a4f111358539ac1e252193ac36a34bf22f3fad
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-14 18:39:51 +0100

    Removed generic from Position interface

--- src/main/java/it/unibo/oop/relario/utils/Position.java
+++ src/main/java/it/unibo/oop/relario/utils/Position.java
@@ -2,9 +2,8 @@ package it.unibo.oop.relario.utils;
 
 /**
  * Interface to handle positions throughout the game.
- * @param <T> The type of the single coordinates.
  */
-public interface Position<T> {
-    public T getX();
-    public T getY();
+public interface Position{
+    public int getX();
+    public int getY();
 }
