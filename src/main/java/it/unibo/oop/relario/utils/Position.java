f38aa89decea9047750aef861b08874ece1333a5
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-14 18:41:29 +0100

    Added JavaDoc comments for Position getters

--- src/main/java/it/unibo/oop/relario/utils/Position.java
+++ src/main/java/it/unibo/oop/relario/utils/Position.java
@@ -4,6 +4,13 @@ package it.unibo.oop.relario.utils;
  * Interface to handle positions throughout the game.
  */
 public interface Position{
+    /**
+     * @return the x coordinate of the current position.
+     */
     public int getX();
+
+    /**
+     * @return the y coordinate of the current position.
+     */
     public int getY();
 }
