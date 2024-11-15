c85f6503744131a01b0067da4a131c60184028c7
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-15 09:21:51 +0100

    Made main class final

--- src/main/java/it/unibo/oop/relario/App.java
+++ src/main/java/it/unibo/oop/relario/App.java
@@ -3,7 +3,7 @@ package it.unibo.oop.relario;
 /**
  * The main class of the application, which starts its execution.
  */
-public class App {
+public final class App {
     /**
      * The main method of the application, from which execution starts.
      * @param args are ignored.
