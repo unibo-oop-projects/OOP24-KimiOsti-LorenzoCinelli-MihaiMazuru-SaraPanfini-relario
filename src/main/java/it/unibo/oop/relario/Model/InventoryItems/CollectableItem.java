136a7d2d9a73876c0ca1a24a8a932d3091d1d93e
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-18 00:13:03 +0100

    deleted class for collectable items

--- src/main/java/it/unibo/oop/relario/Model/InventoryItems/CollectableItem.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.oop.relario.Model.InventoryItems;
-
-public class CollectableItem extends InventoryItemImpl {
-
-    public CollectableItem(final String name, final String description) {
-        super(name, description, EffectType.NONE);
-    }
-    
-}
