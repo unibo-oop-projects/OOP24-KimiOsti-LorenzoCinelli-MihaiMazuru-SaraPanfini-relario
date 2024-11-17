7536b6706437c2a0b19fffedb1b4681ee886842b
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-18 00:21:06 +0100

    added effect field to InventoryItemType enum

--- src/main/java/it/unibo/oop/relario/Model/InventoryItems/InventoryItemType.java
+++ src/main/java/it/unibo/oop/relario/Model/InventoryItems/InventoryItemType.java
@@ -1,8 +1,18 @@
 package it.unibo.oop.relario.Model.InventoryItems;
 
 public enum InventoryItemType {
-    SWORD, BOW, DAGGER, HAMMER,
-    SHIELD, BASICARMOR,
-    POTION, APPLE, AMULET,
-    COIN, GEMSTONE;
+    SWORD(EffectType.DAMAGE), BOW(EffectType.DAMAGE), DAGGER(EffectType.DAMAGE), HAMMER(EffectType.DAMAGE),
+    SHIELD(EffectType.PROTECTION), BASICARMOR(EffectType.PROTECTION),
+    POTION(EffectType.HEALING), APPLE(EffectType.HEALING), AMULET(EffectType.HEALING),
+    COIN(EffectType.NONE), GEMSTONE(EffectType.NONE);
+
+    private final EffectType effect;
+
+    InventoryItemType(EffectType effect) {
+        this.effect = effect;
+    }
+
+    public EffectType getEffect() {
+        return this.effect;
+    }
 }
