c64901fe6cc179f6f4e3a12e8daa07619782fe21
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-17 11:34:35 +0100

    added interface for inventory-items

---
+++ src/main/java/it/unibo/oop/relario/Model/InventoryItems/InventoryItem.java
@@ -0,0 +1,11 @@
+package it.unibo.oop.relario.Model.InventoryItems;
+
+public interface InventoryItem {
+
+    String getName();
+
+    String getDescription();
+
+    EffectType getEffect();
+    
+}
