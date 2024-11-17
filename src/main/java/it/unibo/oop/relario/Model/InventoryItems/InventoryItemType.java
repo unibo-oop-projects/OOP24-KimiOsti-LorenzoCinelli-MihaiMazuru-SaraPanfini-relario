1090ee7a975350de4bd4d288e6cf7713931b4216
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-17 19:39:42 +0100

    added enum for inventory-items types

---
+++ src/main/java/it/unibo/oop/relario/Model/InventoryItems/InventoryItemType.java
@@ -0,0 +1,8 @@
+package it.unibo.oop.relario.Model.InventoryItems;
+
+public enum InventoryItemType {
+    SWORD, BOW, DAGGER, HAMMER,
+    SHIELD, BASICARMOR,
+    POTION, APPLE, AMULET,
+    COIN, GEMSTONE;
+}
