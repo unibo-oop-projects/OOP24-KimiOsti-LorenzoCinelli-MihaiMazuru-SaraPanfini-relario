0eea3a4750160d472574af0a579c61d64faf1889
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-18 22:18:18 +0100

    Added main character interface

---
+++ src/main/java/it/unibo/oop/relario/model/entities/living/MainCharacher.java
@@ -0,0 +1,57 @@
+package it.unibo.oop.relario.model.entities.living;
+
+import it.unibo.oop.relario.model.inventoryitems.Inventory;
+import it.unibo.oop.relario.utils.api.Position;
+import it.unibo.oop.relario.utils.impl.Direction;
+
+/**
+ * Interface to interact with main character.
+ */
+public interface MainCharacher {
+    /**
+     * Reveals the current player position.
+     * @return the current position of the player.
+     */
+    Position getPosition();
+
+    /**
+     * Reveals the current player direction.
+     * @return the current direction the player is facing.
+     */
+    Direction getDirection();
+
+    /**
+     * Asks whether the player is moving.
+     * @return true if the player is moving, false otherwise.
+     */
+    boolean isMoving();
+
+    /**
+     * Sets the player's movement.
+     * @param direction the direction where the player is moving.
+     */
+    void setMovement(Direction direction);
+
+    /**
+     * Stops the player movement.
+     */
+    void stop();
+
+    /**
+     * Updates the player's state, to be called at each unit of time.
+     */
+    void update();
+
+    /**
+     * Handles the player's inventory.
+     * @return the player's inventory.
+     */
+    Inventory handleInventory();
+
+    /**
+     * Inflicts some damage to the player.
+     * @param damage the damage inflicted to the player.
+     * @return true if the player survives the attack, false otherwise.
+     */
+    boolean attacked(int damage);
+}
