b026297ca968c8c35e07e5ed4ce8822980035a0e
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-18 22:05:35 +0100

    Added utility enumeration to list directions

---
+++ src/main/java/it/unibo/oop/relario/utils/impl/Direction.java
@@ -0,0 +1,26 @@
+package it.unibo.oop.relario.utils.impl;
+
+/**
+ * Enumeration to handle direction in game movements.
+ */
+public enum Direction {
+    /**
+     * Towards north.
+     */
+    UP,
+
+    /**
+     * Towards south.
+     */
+    DOWN,
+
+    /**
+     * Towards west.
+     */
+    LEFT,
+
+    /**
+     * Towards east.
+     */
+    RIGHT
+}
