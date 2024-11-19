baa2613d4b71abc23d676cfbdefffc87d6056156
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2024-11-19 11:19:24 +0100

    Modified javadoc

--- src/main/java/it/unibo/oop/relario/model/furniture/FurnitureItem.java
+++ src/main/java/it/unibo/oop/relario/model/furniture/FurnitureItem.java
@@ -3,7 +3,7 @@ package it.unibo.oop.relario.model.furniture;
 import it.unibo.oop.relario.utils.api.Position;
 
 /**
- * Interface for heandling Furniture items.
+ * Interface for handling Furniture items.
  */
 public interface FurnitureItem {
     /**
