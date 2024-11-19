943b60a007acbcbba0921978b9d8a1f6790fdf84
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-19 11:37:05 +0100

    Added getter in entity interface

--- src/main/java/it/unibo/oop/relario/model/entities/Entity.java
+++ src/main/java/it/unibo/oop/relario/model/entities/Entity.java
@@ -1,8 +1,16 @@
 package it.unibo.oop.relario.model.entities;
 
+import it.unibo.oop.relario.utils.api.Position;
+
 /**
  * Interface to interact with every entity.
  */
 public interface Entity {
-    
+
+    /**
+     * Reveals the current entity position.
+     * @return the current position of the entity. 
+     */
+    public Position getPosition();
+
 }
