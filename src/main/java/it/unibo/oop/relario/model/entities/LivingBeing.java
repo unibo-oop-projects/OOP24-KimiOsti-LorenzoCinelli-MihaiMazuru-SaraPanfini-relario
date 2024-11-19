378b3c393986cae0ad6485f8ff6ea73fd4024051
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-19 09:55:16 +0100

    added interface for living beings

---
+++ src/main/java/it/unibo/oop/relario/model/entities/LivingBeing.java
@@ -0,0 +1,11 @@
+package it.unibo.oop.relario.model.entities;
+
+import it.unibo.oop.relario.utils.api.Position;
+
+public interface LivingBeing {
+
+    String getName();
+
+    Position getPosition();
+    
+}
