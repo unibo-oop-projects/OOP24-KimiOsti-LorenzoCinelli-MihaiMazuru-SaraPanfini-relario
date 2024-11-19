5dd06f1b3cd3f0636f67e56ceabfd590e92c6f3b
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-19 11:22:35 +0100

    Added dummy implementation of Controller factory

---
+++ src/main/java/it/unibo/oop/relario/controller/impl/ControllerFactoryImpl.java
@@ -0,0 +1,34 @@
+package it.unibo.oop.relario.controller.impl;
+
+import it.unibo.oop.relario.controller.api.Controller;
+import it.unibo.oop.relario.controller.api.ControllerFactory;
+
+/**
+ * Implementation for the controller factory.
+ */
+public final class ControllerFactoryImpl implements ControllerFactory {
+
+    @Override
+    public Controller createMainMenuController() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createMainMenuController'");
+    }
+
+    @Override
+    public Controller createGameController() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createGameController'");
+    }
+
+    @Override
+    public Controller createInventoryMenuController() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createInventoryMenuController'");
+    }
+
+    @Override
+    public Controller createCombatController() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCombatController'");
+    }
+}
