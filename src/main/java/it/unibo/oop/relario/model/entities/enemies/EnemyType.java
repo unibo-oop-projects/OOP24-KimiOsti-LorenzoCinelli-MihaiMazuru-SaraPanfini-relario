0f9b0080d688f428e0f741d41fe3d673cb475011
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-19 17:03:26 +0100

    added field effect to enemy enum

--- src/main/java/it/unibo/oop/relario/model/entities/enemies/EnemyType.java
+++ src/main/java/it/unibo/oop/relario/model/entities/enemies/EnemyType.java
@@ -1,5 +1,20 @@
 package it.unibo.oop.relario.model.entities.enemies;
 
+import it.unibo.oop.relario.model.inventory.EffectType;
+
 public enum EnemyType {
-    THIEF, SOLDIER, KNIGHT, WIZARD;
+    THIEF(EffectType.NONE),
+    SOLDIER(EffectType.DAMAGE),
+    KNIGHT(EffectType.PROTECTION),
+    WIZARD(EffectType.HEALING);
+
+    private final EffectType effect;
+
+    EnemyType(final EffectType effect) {
+        this.effect = effect;
+    }
+
+    public EffectType getEffect() {
+        return this.effect;
+    }
 }
