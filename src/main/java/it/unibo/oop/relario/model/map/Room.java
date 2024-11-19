d1da38553cb8b2bcdf20f7e89ea0f92b1ec7bb57
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-20 00:19:00 +0100

    added JavaDoc to room interface

--- src/main/java/it/unibo/oop/relario/model/map/Room.java
+++ src/main/java/it/unibo/oop/relario/model/map/Room.java
@@ -5,8 +5,17 @@ import java.util.Optional;
 import it.unibo.oop.relario.model.entities.Entity;
 import it.unibo.oop.relario.utils.api.Position;
 
+/**
+ * Interface for a room in the game map.
+ */
+
 public interface Room {
-    
+
+    /**
+     * Retrieves the content of a specific cell of the room.
+     * @param position of the cell that has to be checked
+     * @return an optional containing the entity in the cell or an empty optional if the cell is empty
+     */
     Optional<Entity> getCellContent(Position position);
 
 }
