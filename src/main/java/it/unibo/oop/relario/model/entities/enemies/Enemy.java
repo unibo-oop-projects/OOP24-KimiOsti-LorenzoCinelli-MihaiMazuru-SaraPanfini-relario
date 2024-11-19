04ccac2b8687c2a22c2717099c77189138e78454
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-19 10:08:24 +0100

    added interface for enemies

---
+++ src/main/java/it/unibo/oop/relario/model/entities/enemies/Enemy.java
@@ -0,0 +1,22 @@
+package it.unibo.oop.relario.model.entities.enemies;
+
+import it.unibo.oop.relario.model.entities.LivingBeing;
+import it.unibo.oop.relario.model.inventoryitems.InventoryItem;
+
+public interface Enemy extends LivingBeing {
+
+    String getDescription();
+
+    int getLife();
+
+    int getDamage();
+
+    DifficultyLevel getDifficulty();
+
+    InventoryItem getReward();
+
+    boolean isMerciful();
+
+    boolean attacked(int playerDamage);
+    
+}
