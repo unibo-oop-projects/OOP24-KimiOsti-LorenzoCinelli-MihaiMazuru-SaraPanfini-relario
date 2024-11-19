82a60b9b289d5f00db3b6c2eb7cd111b991be660
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-19 11:52:35 +0100

    modified room interface

--- src/main/java/it/unibo/oop/relario/model/map/Room.java
+++ src/main/java/it/unibo/oop/relario/model/map/Room.java
@@ -1,10 +1,12 @@
 package it.unibo.oop.relario.model.map;
 
+import java.util.Optional;
+
 import it.unibo.oop.relario.model.entities.Entity;
 import it.unibo.oop.relario.utils.api.Position;
 
 public interface Room {
     
-    Entity getCellContent(Position position);
+    Optional<Entity> getCellContent(Position position);
 
 }
