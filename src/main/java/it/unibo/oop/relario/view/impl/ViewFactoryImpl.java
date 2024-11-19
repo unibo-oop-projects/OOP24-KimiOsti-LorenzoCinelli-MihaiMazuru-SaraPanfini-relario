eb9390a2d392c40eaa575f68a21383a878a941ca
Kimi Osti <kimi.osti@studio.unibo.it>
2024-11-19 11:38:25 +0100

    Added dummy implementation for View Factory

---
+++ src/main/java/it/unibo/oop/relario/view/impl/ViewFactoryImpl.java
@@ -0,0 +1,34 @@
+package it.unibo.oop.relario.view.impl;
+
+import it.unibo.oop.relario.view.api.View;
+import it.unibo.oop.relario.view.api.ViewFactory;
+
+/**
+ * Implementation for the View Factory.
+ */
+public final class ViewFactoryImpl implements ViewFactory {
+
+    @Override
+    public View createMainMenuView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createMainMenuView'");
+    }
+
+    @Override
+    public View createGameView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createGameView'");
+    }
+
+    @Override
+    public View createInventoryMenuView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createInventoryMenuView'");
+    }
+
+    @Override
+    public View createCombatView() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createCombatView'");
+    }
+}
