002748c26c35fe5e572f2940484a9df64b85d3f0
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2024-11-20 12:00:39 +0100

    Created api and impl packages

---
+++ src/main/java/it/unibo/oop/relario/model/entities/furniture/api/WalkableFurniture.java
@@ -0,0 +1,25 @@
+package it.unibo.oop.relario.model.entities.furniture.api;
+
+import it.unibo.oop.relario.model.entities.enemies.Enemy;
+
+/**
+ * Interface for handling walkable furniture items.
+ */
+public interface WalkableFurniture extends FurnitureItem {
+    /**
+     * Says if the furniture contains an enemy.
+     * @return true if the furniture contains an enemt, false otherwise.
+     */
+    boolean hasEnemy();
+
+    /**
+     * Adds an enemy into the furniture.
+     * @param enemy is the enemy to add to the furniture.
+     */
+    void addEnemy(Enemy enemy);
+
+    /**
+     * Removes the enemy from inside the walkable furniture item.
+     */
+    void removeEnemy();
+}
---
+++ src/main/java/it/unibo/oop/relario/model/entities/furniture/impl/ObstructingFurnitureItem.java
@@ -0,0 +1,31 @@
+package it.unibo.oop.relario.model.entities.furniture.impl;
+
+import it.unibo.oop.relario.model.entities.furniture.api.FurnitureItem;
+import it.unibo.oop.relario.utils.api.Position;
+import it.unibo.oop.relario.utils.impl.PositionImpl;
+
+/**
+ * Implementation of obstructing furniture items.
+ */
+public class ObstructingFurnitureItem implements FurnitureItem {
+
+    private final Position pos;
+
+    /**
+     * Initialises a new obstructing furniture item.
+     * @param pos is the position of the furniture item in the map.
+     */
+    public ObstructingFurnitureItem(final Position pos) {
+        this.pos = new PositionImpl(pos.getX(), pos.getY());
+    }
+
+    @Override
+    public final Position getPosition() {
+        return new PositionImpl(this.pos.getX(), this.pos.getY());
+    }
+
+    @Override
+    public final boolean isWalkable() {
+        return false;
+    }
+}
