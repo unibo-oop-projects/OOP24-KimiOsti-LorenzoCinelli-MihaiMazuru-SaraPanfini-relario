fdc54664b37e3f8be19ea1072cc3952c21896b72
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-21 15:53:41 +0100

    added input parameters for public functoions in Interaction class

--- src/main/java/it/unibo/oop/relario/model/Interactions.java
+++ src/main/java/it/unibo/oop/relario/model/Interactions.java
@@ -1,8 +1,10 @@
 package it.unibo.oop.relario.model;
 
+import java.util.Map;
 import java.util.Optional;
 
 import it.unibo.oop.relario.model.entities.Entity;
+import it.unibo.oop.relario.model.entities.furniture.FurnitureItem;
 import it.unibo.oop.relario.utils.api.Position;
 import it.unibo.oop.relario.utils.impl.Direction;
 
@@ -23,9 +25,13 @@ public final class Interactions {
      * Checks if the next position is available.
      * @param pos the initial position of the entity.
      * @param dir the direction the entity is moving.
+     * @param depth the depth of the room
+     * @param width the width of the room
+     * @param entityMap position-entity map
+     * @param furnitureMap position-furniture map
      * @return true if the position is available, false otherwise.
      */
-    public static boolean canMove(final Position pos, final Direction dir) {
+    public static boolean canMove(final Position pos, final Direction dir, final int depth, final int width, final Map<Position, Optional<Entity>> entityMap, final Map<Position, Optional<FurnitureItem>> fornitureMap) {
         return false;
     }
 
@@ -33,9 +39,11 @@ public final class Interactions {
      * Checks if the next position is interactive.
      * @param pos the position of the entity.
      * @param dir the direction the entity is facing.
+     * @param entityMap position-entity map
+     * @param furnitureMap position-furniture map
      * @return true if the position is interactive, false otherwise.
      */
-    public static boolean canInteract(final Position pos, final Direction dir) {
+    public static boolean canInteract(final Position pos, final Direction dir, final Map<Position, Optional<Entity>> entityMap, final Map<Position, Optional<FurnitureItem>> fornitureMap) {
         return false;
     }
 
