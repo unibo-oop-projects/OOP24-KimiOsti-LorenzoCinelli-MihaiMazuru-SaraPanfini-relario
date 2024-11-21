34095a513ff3bbd761f0c155c5ebcbf7bbb109bc
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-11-21 17:27:31 +0100

    Added implementation for canMove method in Interactions class

--- src/main/java/it/unibo/oop/relario/model/Interactions.java
+++ src/main/java/it/unibo/oop/relario/model/Interactions.java
@@ -3,8 +3,8 @@ package it.unibo.oop.relario.model;
 import java.util.Map;
 import java.util.Optional;
 
-import it.unibo.oop.relario.model.entities.Entity;
-import it.unibo.oop.relario.model.entities.furniture.FurnitureItem;
+import it.unibo.oop.relario.model.entities.LivingBeing;
+import it.unibo.oop.relario.model.entities.furniture.api.FurnitureItem;
 import it.unibo.oop.relario.utils.api.Position;
 import it.unibo.oop.relario.utils.impl.Direction;
 
@@ -17,6 +17,19 @@ public final class Interactions {
         throw new UnsupportedOperationException();
     }
 
+    private static boolean isPositionIntoRoomBorder(final Position pos, final int depth, final int width) {
+        return pos.getX() < 0 || pos.getY() < 0 || pos.getX() >= width || pos.getY() >= depth;
+    }
+
+    private static boolean isPositionWithoutEntity(final Position pos, final Map<Position, Optional<LivingBeing>> entityMap) {
+        return !entityMap.get(pos).isPresent();
+    }
+
+    private static boolean isPositionWithoutNonWalkableForniture(final Position pos, 
+    final Map<Position, Optional<FurnitureItem>> furnitureMap) {
+        return furnitureMap.get(pos).isEmpty() || furnitureMap.get(pos).get().isWalkable();
+    }
+
     /**
      * Checks if the next position is available.
      * @param pos the initial position of the entity.
@@ -27,9 +40,12 @@ public final class Interactions {
      * @param furnitureMap position-furniture map
      * @return true if the position is available, false otherwise.
      */
-    public static boolean canMove(final Position pos, final Direction dir, final int depth, final int width, final Map<Position,
-        Optional<Entity>> entityMap, final Map<Position, Optional<FurnitureItem>> furnitureMap) {
-        return false;
+    public static boolean canMove(final Position pos, final Direction dir, final int depth, final int width, 
+    final Map<Position, Optional<LivingBeing>> entityMap, final Map<Position, Optional<FurnitureItem>> furnitureMap) {
+        final Position nextPos = dir.move(pos);
+        return isPositionIntoRoomBorder(nextPos, depth, width)
+        && isPositionWithoutEntity(nextPos, entityMap)
+        && isPositionWithoutNonWalkableForniture(nextPos, furnitureMap);
     }
 
     /**
@@ -40,8 +56,8 @@ public final class Interactions {
      * @param furnitureMap position-furniture map
      * @return true if the position is interactive, false otherwise.
      */
-    public static boolean canInteract(final Position pos, final Direction dir, final Map<Position, Optional<Entity>> entityMap,
-        final Map<Position, Optional<FurnitureItem>> furnitureMap) {
+    public static boolean canInteract(final Position pos, final Direction dir, 
+    final Map<Position, Optional<LivingBeing>> entityMap, final Map<Position, Optional<FurnitureItem>> furnitureMap) {
         return false;
     }
 
