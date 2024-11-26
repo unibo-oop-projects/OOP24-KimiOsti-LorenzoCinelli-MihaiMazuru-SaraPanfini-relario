85fc2c6abc83a8d5f26ec3f49ec897cfc2f89695
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2024-11-26 14:09:04 +0100

    Added minimal implementation for MenuView

--- src/main/java/it/unibo/oop/relario/view/impl/View.java
+++ src/main/java/it/unibo/oop/relario/view/impl/View.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.relario.view.impl;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
 
 /**
@@ -7,4 +9,15 @@ import javax.swing.JFrame;
  */
 public class View extends JFrame {
     private static final long serialVersionUID = 1L;
+
+    /**
+     * Inizializes the frame of the main view.
+     */
+    public View() {
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setExtendedState(MAXIMIZED_BOTH);
+        this.setLayout(new BorderLayout());
+        this.setLocationByPlatform(true);
+        this.setFocusable(true);
+    }
 }
