267c991a45c96f5d28a16d03a9f0b4191fd9c947
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-29 17:03:26 +0100

    added classes to handle dialogues and interactions

---
+++ src/main/java/it/unibo/oop/relario/model/entities/npc/DialoguesGenerator.java
@@ -0,0 +1,35 @@
+package it.unibo.oop.relario.model.entities.npc;
+
+import java.util.List;
+import java.util.Random;
+
+public class DialoguesGenerator {
+
+    private final List<String> defaultDialogues;
+    private final List<String> lootDialogues;
+    private final List<String> noLootDialogues;
+    private final Random random;
+
+    public DialoguesGenerator() {
+        this.random = new Random();
+        this.defaultDialogues = List.of("Oggi è una bella giornata!", "Non ho niente da dire",
+            "Sono impegnato, non  mi disturbare", "Buona fortuna!");
+        this.lootDialogues = List.of("Ho qualcosa che potrebbe esserti utile", "Ecco un regalo per te", 
+            "Forse questo ti potrà servire", "Spero che questo ti aiuti");
+        this.noLootDialogues = List.of("Non ho altro da darti", "Non posso più aiutarti", 
+            "Mi dispiace, quello era tutto");
+    }
+
+    public String getDefaultDialogue() {
+        return this.defaultDialogues.get(random.nextInt(this.defaultDialogues.size()));
+    }
+
+    public String getLootDialogue() {
+        return this.lootDialogues.get(random.nextInt(this.lootDialogues.size()));
+    }
+
+    public String getNoLootDialogue() {
+        return this.noLootDialogues.get(random.nextInt(this.noLootDialogues.size()));
+    }
+    
+}
---
+++ src/main/java/it/unibo/oop/relario/model/entities/npc/InteractionOutput.java
@@ -0,0 +1,24 @@
+package it.unibo.oop.relario.model.entities.npc;
+
+import java.util.Optional;
+
+import it.unibo.oop.relario.model.inventory.InventoryItem;
+
+public class InteractionOutput {
+    private final String dialogue;
+    private final Optional<InventoryItem> item;
+
+    public InteractionOutput(final String dialogue, final Optional<InventoryItem> item) {
+        this.dialogue = dialogue;
+        this.item = item;
+    }
+
+    public String getDialogue() {
+        return this.dialogue;
+    }
+
+    public Optional<InventoryItem> getItem() {
+        return this.item.isPresent() ? this.item : Optional.empty();
+    }
+    
+}
