57862b02eea420429122e017424ba122f235d4f2
Sara Panfini <sara.panfini@studio.unibo.it>
2024-11-29 22:47:36 +0100

    modified living beings generator

--- src/main/java/it/unibo/oop/relario/model/map/LivingBeingsGenerator.java
+++ src/main/java/it/unibo/oop/relario/model/map/LivingBeingsGenerator.java
@@ -1,26 +1,37 @@
 package it.unibo.oop.relario.model.map;
 
 import java.util.Random;
+import java.util.function.Function;
 
+import it.unibo.oop.relario.model.entities.LivingBeing;
 import it.unibo.oop.relario.model.entities.enemies.EnemyFactory;
 import it.unibo.oop.relario.model.entities.enemies.EnemyFactoryImpl;
+import it.unibo.oop.relario.model.entities.npc.NpcFactory;
+import it.unibo.oop.relario.model.entities.npc.NpcFactoryImpl;
 import it.unibo.oop.relario.utils.api.Position;
 
 public final class LivingBeingsGenerator {
     public static final int ENEMIES_NUMBER = 5;
+    public static final int NPC_NUMBER = 3;
 
     private final Random random = new Random();
     private final EnemyFactory enemyFactory = new EnemyFactoryImpl();
+    private final NpcFactory npcFactory = new NpcFactoryImpl();
 
     public void generateLivingBeings(final Room room) {
-        final int placedEnemies = 0;
+        placeCharacters(room, ENEMIES_NUMBER, this.enemyFactory::createRandomEnemy);
+        placeCharacters(room, NPC_NUMBER, this.npcFactory::createRandomNpc);
+    }
 
-        while (placedEnemies < ENEMIES_NUMBER) {
+    private void placeCharacters(final Room room, final int charactersNumber, final Function<Position, LivingBeing> createCharacter) {
+        final int placedCharacters = 0;
+
+        while (placedCharacters < charactersNumber) {
             final Position position = getRandomInnerPosition(room);
             if (room.isPositionValid(position) && room.isCellAvailable(position)) {
-                room.addCharacter(position, this.enemyFactory.createRandomEnemy(position));
+                room.addCharacter(position, createCharacter.apply(position));
             }
-        } 
+        }
     }
 
     private Position getRandomInnerPosition(final Room room) {
