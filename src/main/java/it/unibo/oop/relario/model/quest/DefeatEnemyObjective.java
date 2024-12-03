6c25342563332abc1a0182124ee651750524c1c4
Sara Panfini <sara.panfini@studio.unibo.it>
2024-12-03 09:52:28 +0100

    added examples of objectives

---
+++ src/main/java/it/unibo/oop/relario/model/quest/DefeatEnemyObjective.java
@@ -0,0 +1,11 @@
+package it.unibo.oop.relario.model.quest;
+
+public class DefeatEnemyObjective implements ObjectiveStrategy {
+
+    @Override
+    public boolean check() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'check'");
+    }
+    
+}
