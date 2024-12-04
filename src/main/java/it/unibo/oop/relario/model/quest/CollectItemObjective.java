7a00bf95fdb4c5eac99d5f1ca7b0308f403fc007
Sara Panfini <sara.panfini@studio.unibo.it>
2024-12-04 11:53:37 +0100

    modified quest factory

--- src/main/java/it/unibo/oop/relario/model/quest/CollectItemObjective.java
+++ src/main/java/it/unibo/oop/relario/model/quest/CollectItemObjective.java
@@ -2,15 +2,14 @@ package it.unibo.oop.relario.model.quest;
 
 import it.unibo.oop.relario.model.entities.living.MainCharacter;
 import it.unibo.oop.relario.model.inventory.InventoryItem;
-import it.unibo.oop.relario.model.map.Room;
 
 public final class CollectItemObjective implements ObjectiveStrategy {
 
     private final MainCharacter player;
     private final InventoryItem item; // ??
 
-    public CollectItemObjective(final Room room, final InventoryItem item) {
-        this.player = room.getPlayer();
+    public CollectItemObjective(final MainCharacter player, final InventoryItem item) {
+        this.player = player;
         this.item = item;
     }
 
