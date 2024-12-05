9d8c0148f1cc5dccb5c8aaf8fb612f585fd70ed2
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-12-05 19:11:18 +0100

    Added implementation for constructor and getter methods in inventory controller

--- src/main/java/it/unibo/oop/relario/controller/impl/InventoryControllerImpl.java
+++ src/main/java/it/unibo/oop/relario/controller/impl/InventoryControllerImpl.java
@@ -1,44 +1,61 @@
 package it.unibo.oop.relario.controller.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import it.unibo.oop.relario.controller.api.GameController;
+import it.unibo.oop.relario.controller.api.MainController;
 import it.unibo.oop.relario.controller.api.InventoryController;
 import it.unibo.oop.relario.model.entities.living.MainCharacter;
+import it.unibo.oop.relario.model.inventory.EffectType;
 import it.unibo.oop.relario.model.inventory.InventoryItem;
-import it.unibo.oop.relario.view.impl.MainView;
+import it.unibo.oop.relario.view.api.MainView;
 
 /**
  * 
  */
 public final class InventoryControllerImpl implements InventoryController {
 
-    private final GameController gameController;
+    private final MainController mainController;
     private final MainView mainView;
     private final MainCharacter player;
     private List<InventoryItem> inventory;
 
     /**
-     * @param gameController
+     * @param mainController
      * @param mainView
      * @param player
      */
-    public InventoryControllerImpl(final GameController gameController, final MainView mainView, final MainCharacter player) {
-        this.gameController = gameController;
+    public InventoryControllerImpl(final MainController mainController, final MainView mainView) {
+        this.mainController = mainController;
         this.mainView = mainView;
-        this.player = player;
+        this.player = mainController.getRoom();
         this.inventory = player.getItems();
-        //mainView.add(new InventoryViewImpl(this, mainView));
+    }
+
+    private String getIntensity(InventoryItem item) {
+        if (item.getEffect() == EffectType.NONE) {
+            return "";
+        } else {
+            return " " + item.getIntensity();
+        }
     }
 
     @Override
     public List<String> getItemsNames() {
-        return List.of(); //usa gli stream
+        //return Stream.iterate(1, i -> i + 1).map(i -> i+"\n").collect(Collectors.toList());
+        List<String> temp = new ArrayList<>();
+        for (var item : inventory) {
+            temp.add(item.getName());
+        }
+        return temp;    //usa gli stream
     }
 
     @Override
     public String getItemFullDescription(final int index) {
-        return "";
+        final InventoryItem item = inventory.get(index);
+        return item.getDescription()
+        + ",\nEffect: " + item.getEffect().toString()
+        + this.getIntensity(item);
     }
 
     @Override
@@ -57,5 +74,4 @@ public final class InventoryControllerImpl implements InventoryController {
     public void regress() {
         //tutta la roba nel foglio
     }
-
 }
