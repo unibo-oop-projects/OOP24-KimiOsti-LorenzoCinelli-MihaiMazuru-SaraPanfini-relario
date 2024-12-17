ed5e7ec18f602226817eeb33700be5aeda78607f
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2024-12-17 11:34:20 +0100

    added dummy implementation for inventory view

--- src/main/java/it/unibo/oop/relario/view/impl/InventoryView.java
+++ src/main/java/it/unibo/oop/relario/view/impl/InventoryView.java
@@ -1,10 +1,58 @@
 package it.unibo.oop.relario.view.impl;
 
+import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import it.unibo.oop.relario.controller.api.MainController;
+
+import java.awt.*;
 
 /**
  * View implementation for inventory navigation.
  */
 public class InventoryView extends JPanel {
     private static final long serialVersionUID = 1L;
+
+    // private final MainController controller;
+
+    /**
+     * Initializes the inventory view.
+     * @param controller 
+     */
+    public InventoryView(final MainController controller) {
+        // this.controller = controller;
+        this.setLayout(new BorderLayout(10, 10));
+
+        final JPanel titlePanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        titlePanel.add(new JLabel("Inventory"));
+        this.add(titlePanel, BorderLayout.NORTH);
+
+        final JPanel itemListPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        itemListPanel.add(new JLabel("item list"));
+        final var list = controller.getInventoryController().getItemsNames();
+        for (final var name : list) {
+            itemListPanel.add(new JButton(name));
+        }
+        itemListPanel.add(new JLabel(list.toString()));
+        this.add(itemListPanel, BorderLayout.WEST);
+
+        final JPanel descriptionPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        descriptionPanel.add(new JLabel("item description"));
+        final int index = 0;
+        final var description = controller.getInventoryController().getItemFullDescription(index);
+        descriptionPanel.add(new JLabel(description));
+        this.add(descriptionPanel, BorderLayout.CENTER);
+
+        final JPanel equippedPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        equippedPanel.add(new JLabel("equipped items"));
+        this.add(equippedPanel, BorderLayout.EAST);
+
+        final JPanel commandsPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
+        commandsPanel.add(new JLabel("commands list"));
+        commandsPanel.add(new JLabel("commands list"));
+        commandsPanel.add(new JLabel("commands list"));
+        commandsPanel.add(new JLabel("commands list"));
+        this.add(commandsPanel, BorderLayout.SOUTH);
+    }
 }
