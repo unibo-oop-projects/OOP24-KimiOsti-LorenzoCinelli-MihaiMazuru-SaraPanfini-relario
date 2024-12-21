acee73fb98ebec2edb5e39f510a03502bb9179d2
Kimi Osti <kimi.osti@studio.unibo.it>
2024-12-21 12:19:17 +0100

    Added partial implementation of main character inventory testing

--- src/test/java/it/unibo/oop/relario/model/entities/living/MainCharacterTest.java
+++ src/test/java/it/unibo/oop/relario/model/entities/living/MainCharacterTest.java
@@ -1,9 +1,18 @@
 package it.unibo.oop.relario.model.entities.living;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import it.unibo.oop.relario.model.inventory.EffectType;
+import it.unibo.oop.relario.model.inventory.InventoryItem;
+import it.unibo.oop.relario.model.inventory.InventoryItemFactory;
+import it.unibo.oop.relario.model.inventory.InventoryItemFactoryImpl;
+import it.unibo.oop.relario.utils.impl.Constants;
 import it.unibo.oop.relario.utils.impl.Direction;
 
 /*
@@ -21,6 +30,37 @@ public class MainCharacterTest {
      */
     @Test
     public void testInventory() {
+        final MainCharacter chara = new MainCharacterImpl();
+        final InventoryItemFactory itemFactory = new InventoryItemFactoryImpl();
+        final InventoryItem healing = itemFactory.createRandomItemByEffect(EffectType.HEALING);
+        final InventoryItem weapon = itemFactory.createRandomItemByEffect(EffectType.DAMAGE);
+        final InventoryItem armor = itemFactory.createRandomItemByEffect(EffectType.PROTECTION);
+
+        chara.addToInventory(healing);
+        chara.addToInventory(weapon);
+        chara.addToInventory(armor);
+        assertEquals(
+            List.of(healing, weapon, armor), 
+            chara.getItems()
+        );
+
+        for (int i=0; i<5; i++) {
+            assertTrue(chara.addToInventory(itemFactory.createRandomItem()));
+        }
+        assertFalse(chara.addToInventory(itemFactory.createRandomItem()));
+
+        final int baseAtk = chara.getAttack();
+        assertTrue(chara.useItem(weapon));
+        assertTrue(chara.getAttack() == (baseAtk + weapon.getIntensity()));
+        assertTrue(chara.addToInventory(itemFactory.createRandomItem()));
+
+        assertTrue(chara.useItem(armor));
+        assertTrue(chara.attacked(Constants.DEFAULT_PLAYER_LIFE));
+        assertEquals(armor.getIntensity(), chara.getLife());
+
+        final int prevLife = chara.getLife();
+        assertTrue(chara.discardItem(healing));
+        //assertEquals(chara.getLife(), (prevLife + healing.getIntensity()));
     }
 
     /**
