6dd78e65e564a72e9f2e8bf7f3674f5069e868aa
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-02 16:37:50 +0100

    Added main loop implementation

--- src/main/java/it/unibo/oop/relario/controller/impl/GameControllerImpl.java
+++ src/main/java/it/unibo/oop/relario/controller/impl/GameControllerImpl.java
@@ -1,8 +1,17 @@
 package it.unibo.oop.relario.controller.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.ImageIcon;
+
 import it.unibo.oop.relario.controller.api.GameController;
 import it.unibo.oop.relario.controller.api.MainController;
+import it.unibo.oop.relario.model.entities.Entity;
+import it.unibo.oop.relario.utils.api.Position;
+import it.unibo.oop.relario.utils.impl.Constants;
 import it.unibo.oop.relario.utils.impl.GameState;
+import it.unibo.oop.relario.utils.impl.ResourceLocator;
 import it.unibo.oop.relario.view.api.MainView;
 
 /**
@@ -12,7 +21,7 @@ public final class GameControllerImpl implements GameController {
 
     private final MainController controller;
     private final MainView view;
-    private boolean exploring;
+    private final Thread mainLoop;
 
     /**
      * Constructor for the game controller.
@@ -22,26 +31,61 @@ public final class GameControllerImpl implements GameController {
     public GameControllerImpl(final MainController controller, final MainView view) {
         this.controller = controller;
         this.view = view;
-        this.exploring = false;
+        this.mainLoop = new Thread(this::mainLoop);
     }
 
     @Override
     public void run() {
-        this.exploring = true;
-        this.view.showPanel(GameState.GAME);
-        this.mainLoop();
+        if (this.progressRoom()) {
+            this.mainLoop.start();
+        }
     }
 
     @Override
     public void resume(boolean isExploring) {
-        this.exploring = isExploring;
-        if (!this.exploring) {
+        if (!isExploring) {
             this.view.showPanel(GameState.GAME_OVER);
         } else {
-            this.mainLoop();
+            this.mainLoop.start();
         }
     }
 
     private void mainLoop() {
+        final var gamePanel = this.view.getPanel(GameState.GAME);
+        long prevCycleTime = System.currentTimeMillis();
+        this.view.showPanel(GameState.GAME);
+
+        gamePanel.renderFloor(this.controller.getCurRoom().get().getDimension());
+        gamePanel.renderFurniture(this.processElems(this.controller.getCurRoom().get().getFurniture()));
+
+        while (true) {
+            final long thisCycleTime = System.currentTimeMillis();
+            if (thisCycleTime - prevCycleTime >= Constants.REFRESH_TIME) {
+                prevCycleTime = thisCycleTime;
+                this.controller.getCurRoom().get().update();
+                gamePanel.update(processElems(this.controller.getCurRoom().get().getPopulation()));
+            } else {
+                try {
+                    Thread.sleep(Constants.REFRESH_TIME - System.currentTimeMillis());
+                } catch (InterruptedException e) { 
+                    /**
+                     * Exception is ignored, since interrupting the Thread would mean stopping the main loop execution.
+                     */
+                }
+            }
+        }
+    }
+
+    private Map<Position, ImageIcon> processElems(Map<Position, ? extends Entity> model) {
+        final var res = new HashMap<Position, ImageIcon>();
+        for (var elem : model.entrySet()) {
+            res.put(elem.getKey(), ResourceLocator.getTexture(elem.getValue()));
+        }
+        return Map.copyOf(res);
+    }
+
+    private boolean progressRoom() {
+        this.controller.moveToNextRoom();
+        return this.controller.getCurRoom().isPresent();
     }
 }
