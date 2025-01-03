a71d7382fb3f3279dc19a5869208051f773944f3
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-01-03 17:50:27 +0100

    Created menu manager that initializes in game menu and start menu

---
+++ src/main/java/it/unibo/oop/relario/model/menu/MenuManager.java
@@ -0,0 +1,41 @@
+package it.unibo.oop.relario.model.menu;
+
+/**
+ * Manages the two different menu types.
+ */
+public class MenuManager {
+    
+    private final Menu startMenu;
+    private final Menu inGameMenu;
+
+    /**
+     * Initiliazes the start menu and the in game menu.
+     */
+    public MenuManager() {
+        this.startMenu = new MenuImpl();
+        this.inGameMenu = new MenuImpl();
+        final MenuElement quitElem = new MenuElement(Command.QUIT.getName(), Command.QUIT);
+
+        this.startMenu.addElem(new MenuElement(Command.PLAY.getName(), Command.PLAY));
+        this.startMenu.addElem(quitElem);
+        this.inGameMenu.addElem(new MenuElement(Command.CLOSE.getName(), Command.CLOSE));
+        this.inGameMenu.addElem(quitElem);
+    }
+
+    /**
+     * Retrieves the start menu.
+     * @return the start menu.
+     */
+    public Menu getStartMenu() {
+        return this.startMenu;
+    }
+
+    /**
+     * Retrieves the in game menu.
+     * @return the in game menu.
+     */
+    public Menu getInGameMenu() {
+        return this.inGameMenu;
+    }
+
+}
