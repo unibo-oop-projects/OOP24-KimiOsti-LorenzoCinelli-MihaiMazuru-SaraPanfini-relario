db7b0543435642c215a57d8e4e7f36eb34124905
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-01-03 17:57:30 +0100

    Refactoring menu view

--- src/main/java/it/unibo/oop/relario/view/impl/MainMenuView.java
+++ src/main/java/it/unibo/oop/relario/view/impl/MainMenuView.java
@@ -1,14 +1,22 @@
 package it.unibo.oop.relario.view.impl;
 
 import java.awt.BorderLayout;
+import java.awt.Frame;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.WindowEvent;
+import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 import it.unibo.oop.relario.controller.api.MainController;
+import it.unibo.oop.relario.model.menu.Command;
+import it.unibo.oop.relario.model.menu.MenuElement;
+import it.unibo.oop.relario.view.api.MainView;
 
 /**
  * View implementation for the main menu.
@@ -16,37 +24,58 @@ import it.unibo.oop.relario.controller.api.MainController;
 public final class MainMenuView extends JPanel {
 
     private static final long serialVersionUID = 1L;
-    private final transient MainController controller;
+    private final static int INSETS = 3;
+    private final static String GAME_NAME  = "RELARIO";
+    private final MainView view;
+    private final MainController controller;
 
     /**
-     * Initializes the main menu.
-     * @param controller is the controller of the main menu.
+     * Initializes a new menu view.
+     * @param view is the main view that contains all the game panels.
+     * @param elements are the menu elements that need to be added to the view.
      */
-    public MainMenuView(final MainController controller) {
+    public MainMenuView(final MainView view, final List<MenuElement> elements, 
+    final MainController controller) {
+        this.view = view;
         this.controller = controller;
-        final JPanel myPanel = new JPanel(new GridBagLayout());
-        final GridBagConstraints c = new GridBagConstraints();
-        c.gridy = 0;
-        c.insets = new Insets(1, 1, 1, 1);
-        c.fill = GridBagConstraints.HORIZONTAL;
+        this.setLayout(new GridBagLayout());
+        final GridBagConstraints gridc = new GridBagConstraints();
+        gridc.gridy = 0;
+        gridc.insets = new Insets(INSETS, INSETS, INSETS, INSETS);
+        gridc.fill = GridBagConstraints.CENTER;
 
-        final JButton play = new JButton("PLAY");
-        myPanel.add(play, c);
-        c.gridy++;
+        final JLabel title = new JLabel(GAME_NAME);
+        this.add(title);
+        gridc.gridy++;
 
-        play.addActionListener(e -> { 
-            this.controller.getMainMenuController().progress(); 
-        });
+        for (final var elem: elements) {
+            this.add(createButton(elem));
+            gridc.gridy++;
+        }
 
-        final JButton quit = new JButton("QUIT");
-        myPanel.add(quit, c);
-        c.gridy++;
-
-        quit.addActionListener(e -> { 
-            this.controller.getMainMenuController().exit(); 
-        });
-
-        this.setLayout(new BorderLayout());
-        this.add(myPanel, BorderLayout.CENTER);
+        this.add(this, BorderLayout.CENTER);
     }
+
+    private JButton createButton(MenuElement elem) {
+        final JButton mybutton = new JButton(elem.getElemName());
+        mybutton.addActionListener(e -> {
+            if (e.getActionCommand().equals(Command.PLAY.getName())) {
+                this.controller.getGameController().run();
+            } else if (e.getActionCommand().equals(Command.CLOSE.getName())) {
+                this.view.showPreviousPanel();
+            } else if (e.getActionCommand().equals(Command.QUIT.getName())) {
+                final int dialogResult = JOptionPane.showConfirmDialog(this,
+                    "Are you sure you want to quit the game?", "Warning",
+                    JOptionPane.YES_NO_OPTION);
+
+                if (dialogResult == JOptionPane.YES_OPTION) {
+                    for (final var f : Frame.getFrames()) {
+                        f.dispatchEvent(new WindowEvent(f, WindowEvent.WINDOW_CLOSING));
+                    }
+                }
+            }
+        });
+        return mybutton;
+    }
+
 }
