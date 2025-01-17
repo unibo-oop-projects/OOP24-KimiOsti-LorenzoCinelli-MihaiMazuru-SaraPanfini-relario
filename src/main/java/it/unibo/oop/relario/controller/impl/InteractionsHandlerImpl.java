038898225180f1add1e0ecbe33c7204b3f8f21fb
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-17 16:54:44 +0100

    Added partial implementation of interactions handler

--- src/main/java/it/unibo/oop/relario/controller/impl/InteractionsHandlerImpl.java
+++ src/main/java/it/unibo/oop/relario/controller/impl/InteractionsHandlerImpl.java
@@ -2,6 +2,7 @@ package it.unibo.oop.relario.controller.impl;
 
 import it.unibo.oop.relario.controller.api.InteractionsHandler;
 import it.unibo.oop.relario.controller.api.MainController;
+import it.unibo.oop.relario.model.Interactions;
 import it.unibo.oop.relario.model.map.Room;
 
 /**
@@ -11,7 +12,21 @@ public final class InteractionsHandlerImpl implements InteractionsHandler {
 
     @Override
     public void handleInteraction(final Room curRoom, final MainController controller) {
-        /* [TODO] : add method implementation */
+        if (
+            curRoom.getPlayer().getPosition().isPresent()
+            && Interactions.canInteract(
+                curRoom.getPlayer().getPosition().get(),
+                curRoom.getPlayer().getDirection(),
+                curRoom.getPopulation(),
+                curRoom.getFurniture()
+            )
+        ) {
+            if (curRoom.getPlayer().getPosition().get().equals(curRoom.getExit())) {
+                controller.moveToNextRoom();
+            } else {
+                /* [TODO]: handle interaction scenarios */
+            }
+        }
 
         controller.getGameController().resume(controller.getCurRoom().isPresent());
     }
