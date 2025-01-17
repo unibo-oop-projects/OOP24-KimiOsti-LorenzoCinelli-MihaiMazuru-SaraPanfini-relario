3dba09f0ee847ec51b1e4cd992671dd02a73a777
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-17 20:26:20 +0100

    Fixed according to new controller interface

--- src/test/java/it/unibo/oop/relario/controller/InventoryControllerTest.java
+++ src/test/java/it/unibo/oop/relario/controller/InventoryControllerTest.java
@@ -66,22 +66,22 @@ final class InventoryControllerTest {
 
         assertEquals(0, inventoryController.getSelectedItemIndex());
         assertEquals("Un ciondolo antico e luminoso che emana un'aura di guarigione,\nEffetto: Cura 15",
-        inventoryController.getItemFullDescription(inventoryController.getSelectedItemIndex()));
+        inventoryController.getItemFullDescription());
         
         inventoryController.notify(Event.NEXT_ITEM);
         assertEquals(1, inventoryController.getSelectedItemIndex());
         assertEquals("Una gemma scintillante di rara bellezza,\nEffetto: Nessuno",
-        inventoryController.getItemFullDescription(inventoryController.getSelectedItemIndex()));
+        inventoryController.getItemFullDescription());
 
         inventoryController.notify(Event.NEXT_ITEM);
         assertEquals(2, inventoryController.getSelectedItemIndex());
-        assertEquals("Un'armatura leggera che offre protezione di base,\nEffetto: Protezione 5",
-        inventoryController.getItemFullDescription(inventoryController.getSelectedItemIndex()));
+        assertEquals("Un'armatura leggera che offre protezione di base,\nEffetto: Protezione 5\nDurabilità: 3",
+        inventoryController.getItemFullDescription());
 
         inventoryController.notify(Event.PREVIOUS_ITEM);
         assertEquals(1, inventoryController.getSelectedItemIndex());
         assertEquals("Una gemma scintillante di rara bellezza,\nEffetto: Nessuno",
-        inventoryController.getItemFullDescription(inventoryController.getSelectedItemIndex()));
+        inventoryController.getItemFullDescription());
 
         assertEquals(List.of("Amuleto", "Pietra preziosa", "Armatura semplice"),
         this.inventoryController.getItemsNames());
@@ -106,8 +106,8 @@ final class InventoryControllerTest {
         assertEquals(List.of("Amuleto", "Pietra preziosa", "Scudo"),
         this.inventoryController.getItemsNames());
         assertEquals(2, this.inventoryController.getSelectedItemIndex());
-        assertEquals("Uno scudo robusto e affidabile, capace di bloccare colpi potenti,\nEffetto: Protezione 10",
-        this.inventoryController.getItemFullDescription(this.inventoryController.getSelectedItemIndex()));
+        assertEquals("Uno scudo robusto e affidabile, capace di bloccare colpi potenti,\nEffetto: Protezione 10\nDurabilità: 5",
+        this.inventoryController.getItemFullDescription());
 
         this.inventoryController.notify(Event.NEXT_ITEM);
         assertEquals(0, this.inventoryController.getSelectedItemIndex());
@@ -115,7 +115,7 @@ final class InventoryControllerTest {
         assertEquals(List.of("Pietra preziosa", "Scudo"),
         this.inventoryController.getItemsNames());
         assertEquals("Una gemma scintillante di rara bellezza,\nEffetto: Nessuno",
-        this.inventoryController.getItemFullDescription(inventoryController.getSelectedItemIndex()));
+        this.inventoryController.getItemFullDescription());
 
         this.inventoryController.notify(Event.NEXT_ITEM);
         assertEquals(1, this.inventoryController.getSelectedItemIndex());
@@ -123,8 +123,8 @@ final class InventoryControllerTest {
         assertEquals(List.of("Pietra preziosa", "Armatura semplice"),
         this.inventoryController.getItemsNames());
         assertEquals(1, this.inventoryController.getSelectedItemIndex());
-        assertEquals("Un'armatura leggera che offre protezione di base,\nEffetto: Protezione 5",
-        inventoryController.getItemFullDescription(inventoryController.getSelectedItemIndex()));
+        assertEquals("Un'armatura leggera che offre protezione di base,\nEffetto: Protezione 5\nDurabilità: 3",
+        inventoryController.getItemFullDescription());
 
         this.inventoryController.notify(Event.NEXT_ITEM);
         assertEquals(0, this.inventoryController.getSelectedItemIndex());
@@ -132,8 +132,8 @@ final class InventoryControllerTest {
         assertEquals(List.of("Armatura semplice"),
         this.inventoryController.getItemsNames());
         assertEquals(0, this.inventoryController.getSelectedItemIndex());
-        assertEquals("Un'armatura leggera che offre protezione di base,\nEffetto: Protezione 5",
-        inventoryController.getItemFullDescription(inventoryController.getSelectedItemIndex()));
+        assertEquals("Un'armatura leggera che offre protezione di base,\nEffetto: Protezione 5\nDurabilità: 3",
+        inventoryController.getItemFullDescription());
     }
 
     /**
