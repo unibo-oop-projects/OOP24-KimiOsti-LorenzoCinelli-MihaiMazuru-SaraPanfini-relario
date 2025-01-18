644d60f62e0cf6967be417c0c65699cb1ee4ca53
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-18 17:52:15 +0100

    Added empty implementation of interactions handler tester class

---
+++ src/test/java/it/unibo/oop/relario/controller/InteractionsHandlerTest.java
@@ -0,0 +1,17 @@
+package it.unibo.oop.relario.controller;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class for the game's interaction handler.
+ */
+public class InteractionsHandlerTest {
+
+    /**
+     * A method to test the interaction handling motor.
+     */
+    @Test
+    void testInteractionScenarios() {
+
+    }
+}
