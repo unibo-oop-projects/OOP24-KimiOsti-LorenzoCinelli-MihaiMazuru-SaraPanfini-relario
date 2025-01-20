8f46cf4e9b1cf7cba1eeb2b93c217b78e52d8ed2
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-01-20 22:12:33 +0100

    Added test for MenuImpl class

--- src/test/java/it/unibo/oop/relario/model/menu/MenuTest.java
+++ src/test/java/it/unibo/oop/relario/model/menu/MenuTest.java
@@ -2,21 +2,45 @@ package it.unibo.oop.relario.model.menu;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MenuTest {
+
+    private MenuElement elemClose;
+    private MenuElement elemPlay;
+    private MenuElement elemQuit;
+
+    @BeforeEach
+    void setUp() {
+        elemClose = new MenuElement(Command.CLOSE);
+        elemPlay = new MenuElement(Command.PLAY);
+        elemQuit = new MenuElement(Command.QUIT);
+    }
     
     @Test
     void testMenuElement() {
-        final MenuElement elemClose = new MenuElement(Command.CLOSE);
-        final MenuElement elemPlay = new MenuElement(Command.PLAY);
-        final MenuElement elemQuit = new MenuElement(Command.QUIT);
-
         assertEquals(elemClose.getElemCommad(), Command.CLOSE);
         assertEquals(elemClose.getElemName(), Command.CLOSE.getName());
         assertEquals(elemPlay.getElemCommad(), Command.PLAY);
         assertEquals(elemPlay.getElemName(), Command.PLAY.getName());
         assertEquals(elemQuit.getElemCommad(), Command.QUIT);
         assertEquals(elemQuit.getElemName(), Command.QUIT.getName());
+    }
+
+    @Test
+    void testMenuImpl() {
+        final MenuImpl menu = new MenuImpl();
+
+        assertEquals(menu.getElem(), new ArrayList<>());
+        menu.addElem(elemPlay);
+        menu.addElem(elemClose);
+        menu.addElem(elemQuit);
+        assertEquals(menu.getElem(), List.of(elemPlay, elemClose, elemQuit));
+        menu.addElem(elemClose);
+        assertEquals(menu.getElem(), List.of(elemPlay, elemClose, elemQuit, elemClose));
     }
 }
