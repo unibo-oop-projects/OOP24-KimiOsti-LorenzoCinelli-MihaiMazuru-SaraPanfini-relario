3563455786c0798800359d54dade02c7803a7f30
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-20 18:54:08 +0100

    Created interface and empty implementation for the central combat scene

---
+++ src/main/java/it/unibo/oop/relario/view/api/CombatScene.java
@@ -0,0 +1,13 @@
+package it.unibo.oop.relario.view.api;
+
+/**
+ * The interface for the central scene of combat environments.
+ */
+public interface CombatScene {
+
+    /**
+     * Updates the content of the scene.
+     */
+    void update();
+
+}
---
+++ src/main/java/it/unibo/oop/relario/view/impl/CombatSceneImpl.java
@@ -0,0 +1,18 @@
+package it.unibo.oop.relario.view.impl;
+
+import javax.swing.JPanel;
+
+import it.unibo.oop.relario.view.api.CombatScene;
+
+/**
+ * Implementation for the central scene of combat environments.
+ */
+public class CombatSceneImpl extends JPanel implements CombatScene {
+
+    @Override
+    public void update() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+    
+}
