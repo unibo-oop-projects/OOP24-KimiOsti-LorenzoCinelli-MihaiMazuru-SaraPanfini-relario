e05406a79af036709d89a1a3f890cd842b06989f
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-20 18:58:21 +0100

    Added implementation of combat scene constructor

--- src/main/java/it/unibo/oop/relario/view/impl/CombatSceneImpl.java
+++ src/main/java/it/unibo/oop/relario/view/impl/CombatSceneImpl.java
@@ -2,12 +2,26 @@ package it.unibo.oop.relario.view.impl;
 
 import javax.swing.JPanel;
 
+import it.unibo.oop.relario.controller.api.CombatController;
 import it.unibo.oop.relario.view.api.CombatScene;
 
 /**
  * Implementation for the central scene of combat environments.
  */
 public class CombatSceneImpl extends JPanel implements CombatScene {
+
+    private final CombatController controller;
+    private final JPanel enemyInfo;
+    private final JPanel enemyImage;
+    private final JPanel playerInfo;
+
+    public CombatSceneImpl(final CombatController controller) {
+        this.controller = controller;
+
+        this.enemyInfo = new JPanel();
+        this.enemyImage = new JPanel();
+        this.playerInfo = new JPanel();
+    }
 
     @Override
     public void update() {
