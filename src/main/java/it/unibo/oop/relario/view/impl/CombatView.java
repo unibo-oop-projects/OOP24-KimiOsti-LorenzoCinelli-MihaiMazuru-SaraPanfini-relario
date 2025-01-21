cae56ef226e482409db0ae1858b8c9be786bc631
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-21 10:32:10 +0100

    Added method to generate central panel for command buttons

--- src/main/java/it/unibo/oop/relario/view/impl/CombatView.java
+++ src/main/java/it/unibo/oop/relario/view/impl/CombatView.java
@@ -2,7 +2,10 @@ package it.unibo.oop.relario.view.impl;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.util.List;
 
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,23 +26,27 @@ public class CombatView extends JPanel {
     private static final Color TEXT_COLOR = Color.WHITE;
     private static final double SIDE_COMPONENTS_RATIO = 0.25;
     private static final double FONT_TO_PANEL_RATIO = 0.25;
+    private static final boolean ATTACKING = false;
+    private static final boolean BEGGING_MERCY = true;
 
     private final CombatController controller;
     private final JPanel upperPadding;
     private final CombatScene centralScene;
     private final JPanel commands;
     private final JPanel message;
+    private final Font font;
 
     public CombatView(final CombatController controller) {
         this.setBackground(BACKGROUND_COLOR);
 
         this.controller = controller;
+        this.font = ResourceLocator.getGameFont(Constants.MONOSPACE_FONT);
 
         this.upperPadding = new JPanel();
         this.setupPanel(this.upperPadding);
         this.centralScene = new CombatScene(this.controller);
         this.setupPanel(this.centralScene);
-        this.commands = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        this.commands = this.createCommandPanel();
         this.setupPanel(this.commands);
         this.message = new JPanel(new FlowLayout(FlowLayout.CENTER));
         this.setupPanel(this.message);
@@ -85,11 +92,28 @@ public class CombatView extends JPanel {
         final var label = new JLabel();
         label.setBackground(BACKGROUND_COLOR);
         label.setForeground(TEXT_COLOR);
-        label.setFont(ResourceLocator.getGameFont(Constants.MONOSPACE_FONT).deriveFont(
+        label.setFont(this.font.deriveFont(
             (float) (this.message.getPreferredSize().getHeight() * FONT_TO_PANEL_RATIO)
         ));
         label.setText(msg);
         this.refresh(this.message);
+    }
+
+    private JPanel createCommandPanel() {
+        final var panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        final var attack = new JButton("Attacca");
+        attack.addActionListener(e -> this.controller.handleCombatAction(ATTACKING));
+        final var inventory = new JButton("Inventario");
+        final var mercy = new JButton("Chiedi pietà");
+        mercy.addActionListener(e -> this.controller.handleCombatAction(BEGGING_MERCY));
+        final var buttons = List.of(attack, inventory, mercy);
+
+        buttons.forEach(e -> {
+            panel.add(e);
+            e.setFont(this.font);
+        });
+
+        return panel;
     }
 
 }
