90bf450a12da87d96cf867d02d6857d5e5ebfae8
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-21 10:21:50 +0100

    Implemented update method and modified constructor to support modified components layout managers

--- src/main/java/it/unibo/oop/relario/view/impl/CombatView.java
+++ src/main/java/it/unibo/oop/relario/view/impl/CombatView.java
@@ -1,12 +1,17 @@
 package it.unibo.oop.relario.view.impl;
 
 import java.awt.Color;
-import java.awt.Font;
+import java.awt.FlowLayout;
 
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import it.unibo.oop.relario.controller.api.CombatController;
+import it.unibo.oop.relario.utils.impl.Constants;
+import it.unibo.oop.relario.utils.impl.ResourceLocator;
+
+/* [TODO]: aggiungere il contenuto del component dei comandi */
 
 /**
  * View implementation for the combat phase of the game.
@@ -17,14 +22,13 @@ public class CombatView extends JPanel {
     private static final Color BACKGROUND_COLOR = Color.BLACK;
     private static final Color TEXT_COLOR = Color.WHITE;
     private static final double SIDE_COMPONENTS_RATIO = 0.25;
+    private static final double FONT_TO_PANEL_RATIO = 0.25;
 
     private final CombatController controller;
     private final JPanel upperPadding;
     private final CombatScene centralScene;
     private final JPanel commands;
     private final JPanel message;
-
-    /* [TODO]: aggiungere il contenuto ai component e scrivere il metodo update */
 
     public CombatView(final CombatController controller) {
         this.setBackground(BACKGROUND_COLOR);
@@ -35,20 +39,20 @@ public class CombatView extends JPanel {
         this.setupPanel(this.upperPadding);
         this.centralScene = new CombatScene(this.controller);
         this.setupPanel(this.centralScene);
-        this.commands = new JPanel();
+        this.commands = new JPanel(new FlowLayout(FlowLayout.CENTER));
         this.setupPanel(this.commands);
-        this.message = new JPanel();
+        this.message = new JPanel(new FlowLayout(FlowLayout.CENTER));
         this.setupPanel(this.message);
 
         this.resizePanels();
-        this.update();
     }
 
     /**
      * Updates the combat view.
      */
     public void update() {
-
+        this.centralScene.update();
+        this.updateMessage(this.controller.getCombatState());
     }
 
     private void setupPanel(final JPanel panel) {
@@ -74,6 +78,18 @@ public class CombatView extends JPanel {
     private void refresh(final JComponent component) {
         component.revalidate();
         component.repaint();
+    }
+
+    private void updateMessage(final String msg) {
+        this.message.removeAll();
+        final var label = new JLabel();
+        label.setBackground(BACKGROUND_COLOR);
+        label.setForeground(TEXT_COLOR);
+        label.setFont(ResourceLocator.getGameFont(Constants.MONOSPACE_FONT).deriveFont(
+            (float) (this.message.getPreferredSize().getHeight() * FONT_TO_PANEL_RATIO)
+        ));
+        label.setText(msg);
+        this.refresh(this.message);
     }
 
 }
