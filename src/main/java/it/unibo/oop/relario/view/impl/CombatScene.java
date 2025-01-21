ca0b5f2e849e858828c5ca204019ccb619f34b37
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-21 11:43:29 +0100

    Implemented constructor and update method

--- src/main/java/it/unibo/oop/relario/view/impl/CombatScene.java
+++ src/main/java/it/unibo/oop/relario/view/impl/CombatScene.java
@@ -1,30 +1,74 @@
 package it.unibo.oop.relario.view.impl;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+
 import javax.swing.JPanel;
 
 import it.unibo.oop.relario.controller.api.CombatController;
+import it.unibo.oop.relario.utils.impl.Constants;
+import it.unibo.oop.relario.utils.impl.ResourceLocator;
+
+/* [TODO]: implementare la classe */
 
 /**
  * Implementation for the central scene of combat environments.
  */
 public class CombatScene extends JPanel {
 
+    private final static Color BACKGROUND_COLOR = Color.BLACK;
+    private final static Color TEXT_COLOR = Color.WHITE;
+
     private final CombatController controller;
-    private final JPanel enemyInfo;
-    private final JPanel enemyImage;
-    private final JPanel playerInfo;
+    private final Font font;
 
     public CombatScene(final CombatController controller) {
+        this.setLayout(new BorderLayout());
         this.controller = controller;
-
-        this.enemyInfo = new JPanel();
-        this.enemyImage = new JPanel();
-        this.playerInfo = new JPanel();
+        this.font = ResourceLocator.getGameFont(Constants.MONOSPACE_FONT);
     }
 
     public void update() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'update'");
+        this.removeAll();
+        this.add(
+            this.getEnemyInfoPanel(
+                this.controller.getEnemyLife(),
+                this.controller.getEnemyName()
+            ),
+            BorderLayout.NORTH
+        );
+        this.add(
+            this.getEnemyImagePanel(null /* [TODO]: aggiungere immagine del nemico */),
+            BorderLayout.CENTER
+        );
+        this.add(
+            this.getPlayerInfoPanel(
+                this.controller.getPlayerLife(),
+                this.controller.getItem(),
+                this.controller.getArmor()
+            ),
+            BorderLayout.SOUTH
+        );
+    }
+
+    private JPanel getEnemyInfoPanel(final int enemyLife, final String enemyName) {
+        final var panel = new JPanel();
+
+        return panel;
+    }
+
+    private JPanel getEnemyImagePanel(final Image enemy) {
+        final var panel = new JPanel();
+
+        return panel;
+    }
+
+    private JPanel getPlayerInfoPanel(final int playerLife, final String weapon, final String armor) {
+        final var panel = new JPanel();
+
+        return panel;
     }
     
 }
