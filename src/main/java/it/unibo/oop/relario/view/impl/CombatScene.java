3e2168bb4fb4de7c605c9982c48bc90dc80f1455
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-21 11:54:32 +0100

    Modified getter for info panel and update method accordingly

--- src/main/java/it/unibo/oop/relario/view/impl/CombatScene.java
+++ src/main/java/it/unibo/oop/relario/view/impl/CombatScene.java
@@ -3,15 +3,17 @@ package it.unibo.oop.relario.view.impl;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.Image;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import it.unibo.oop.relario.controller.api.CombatController;
 import it.unibo.oop.relario.utils.impl.Constants;
 import it.unibo.oop.relario.utils.impl.ResourceLocator;
 
-/* [TODO]: implementare la classe */
+/* [TODO]: implementare getter per image panel */
 
 /**
  * Implementation for the central scene of combat environments.
@@ -20,6 +22,8 @@ public class CombatScene extends JPanel {
 
     private final static Color BACKGROUND_COLOR = Color.BLACK;
     private final static Color TEXT_COLOR = Color.WHITE;
+    private final static int INFO_ROWS = 1;
+    private final static int INFO_COLS = 3;
 
     private final CombatController controller;
     private final Font font;
@@ -33,8 +37,8 @@ public class CombatScene extends JPanel {
     public void update() {
         this.removeAll();
         this.add(
-            this.getEnemyInfoPanel(
-                this.controller.getEnemyLife(),
+            this.getInfoPanel(
+                String.valueOf(this.controller.getEnemyLife()),
                 this.controller.getEnemyName()
             ),
             BorderLayout.NORTH
@@ -44,28 +48,37 @@ public class CombatScene extends JPanel {
             BorderLayout.CENTER
         );
         this.add(
-            this.getPlayerInfoPanel(
-                this.controller.getPlayerLife(),
+            this.getInfoPanel(
+                String.valueOf(this.controller.getPlayerLife()),
                 this.controller.getItem(),
                 this.controller.getArmor()
             ),
             BorderLayout.SOUTH
         );
+
+        this.refresh();
     }
 
-    private JPanel getEnemyInfoPanel(final int enemyLife, final String enemyName) {
-        final var panel = new JPanel();
+    private void refresh() {
+        this.revalidate();
+        this.repaint();
+    }
+
+    private JPanel getInfoPanel(final String ... info) {
+        final var panel = new JPanel(new GridLayout(INFO_ROWS, INFO_COLS));
+        panel.setBackground(BACKGROUND_COLOR);
+        for (int i = 0; i < info.length && i < INFO_COLS; i++) {
+            final var text = new JLabel();
+            text.setBackground(BACKGROUND_COLOR);
+            text.setForeground(TEXT_COLOR);
+            text.setFont(font);
+            panel.add(text);
+        }
 
         return panel;
     }
 
     private JPanel getEnemyImagePanel(final Image enemy) {
-        final var panel = new JPanel();
-
-        return panel;
-    }
-
-    private JPanel getPlayerInfoPanel(final int playerLife, final String weapon, final String armor) {
         final var panel = new JPanel();
 
         return panel;
