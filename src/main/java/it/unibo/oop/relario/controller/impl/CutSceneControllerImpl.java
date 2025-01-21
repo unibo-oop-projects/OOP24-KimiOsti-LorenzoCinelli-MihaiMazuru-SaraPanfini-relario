9c2a9b48d08362b0ca4b2af372a02c23f59da4bb
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-21 18:05:52 +0100

    Implemented show method

--- src/main/java/it/unibo/oop/relario/controller/impl/CutSceneControllerImpl.java
+++ src/main/java/it/unibo/oop/relario/controller/impl/CutSceneControllerImpl.java
@@ -3,13 +3,18 @@ package it.unibo.oop.relario.controller.impl;
 import it.unibo.oop.relario.controller.api.CutSceneController;
 import it.unibo.oop.relario.controller.api.MainController;
 import it.unibo.oop.relario.utils.impl.GameState;
+import it.unibo.oop.relario.view.api.CutSceneView;
 import it.unibo.oop.relario.view.api.MainView;
 
-public class CutSceneControllerImpl implements CutSceneController {
+/**
+ * Implementation of {@link CutSceneController}.
+ */
+public final class CutSceneControllerImpl implements CutSceneController {
 
     private final MainController controller;
     private final MainView view;
-    
+    private final CutSceneView cutSceneView;
+
     /**
      * 
      * @param controller
@@ -18,19 +23,19 @@ public class CutSceneControllerImpl implements CutSceneController {
     public CutSceneControllerImpl(final MainController controller, final MainView view) {
         this.controller = controller;
         this.view = view;
+        this.cutSceneView = null;
     }
-    
+
     @Override
     public void show(final GameState nextState) {
         final var currentState = this.view.getCurrentPanel();
         if (currentState == GameState.MENU) {
-            //TODO mostra schermata iniziale
+            this.cutSceneView.showStartScene();
         } else {
-            //TODO carica schermata comune
-            switch (nextState) { //TODO mostra sotto schermata specifica
-                case GAME -> { }
-                case VICTORY -> { }
-                case GAME_OVER -> { }
+            switch (nextState) {
+                case GAME -> this.cutSceneView.showNextRoomScene();
+                case VICTORY -> this.cutSceneView.showVictoryScene();
+                case GAME_OVER -> this.cutSceneView.showDefeatScene();
                 default -> { }
             }
         }
