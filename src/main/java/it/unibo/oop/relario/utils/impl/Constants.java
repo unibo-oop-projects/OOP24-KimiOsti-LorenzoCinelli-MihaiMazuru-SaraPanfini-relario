6e4a5521411590e84c78390e265157edbee061d4
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-22 18:11:13 +0100

    Added constants regarding fonts

--- src/main/java/it/unibo/oop/relario/utils/impl/Constants.java
+++ src/main/java/it/unibo/oop/relario/utils/impl/Constants.java
@@ -8,7 +8,6 @@ public final class Constants {
      * Default player's initial life.
      */
     public static final int DEFAULT_PLAYER_LIFE = 80;
-
     /**
      * Default player's base attack.
      */
@@ -20,9 +19,17 @@ public final class Constants {
     public static final int REFRESH_TIME = 125;
 
     /**
-     * Game's font name.
+     * Game font base URL.
+     */
+    public static final String FONT_URL = "font/";
+    /**
+     * Game's monospace font name.
      */
     public static final String MONOSPACE_FONT = "monogram";
+    /**
+     * Font file extension.
+     */
+    public static final String FONT_EXTENSION = ".ttf";
 
     private Constants() {
         throw new UnsupportedOperationException();
