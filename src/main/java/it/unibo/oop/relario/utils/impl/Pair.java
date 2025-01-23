3d310220ea4f3bfa8a217ab1a6bb5824e3344027
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-24 00:27:52 +0100

    Fixed pair

--- src/main/java/it/unibo/oop/relario/utils/impl/Pair.java
+++ src/main/java/it/unibo/oop/relario/utils/impl/Pair.java
@@ -4,32 +4,43 @@ import java.util.Objects;
 
 /**
  * Implementation of Pair.
+ * @param <X> the type of the first element.
+ * @param <Y> the type of the second element.
  */
-
- public final class Pair<X, Y> {
+public final class Pair<X, Y> {
 
     private final X x;
     private final Y y;
 
     /**
-     * Constructor for room's dimension, given height and width.
-     * @param height of the room
-     * @param width of the room
+     * Constructor for a pair of elements.
+     * @param x the first element.
+     * @param y the second element.
      */
     public Pair(final X x, final Y y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns the first element of the pair.
+     * @return the first element of the pair.
+     */
     public X getX() {
         return this.x;
     }
 
+    /**
+     * Returns the second element of the pair.
+     * @return the second element of the pair.
+     */
     public Y getY() {
         return this.y;
     }
 
+    /* Need to suppress rawtypes warnings due to the explicit cast from Object to Pair */
     @SuppressWarnings("rawtypes")
+    @Override
     public boolean equals(final Object obj) {
         if (this == obj) {
             return true;
@@ -41,6 +52,7 @@ import java.util.Objects;
         return Objects.equals(this.getX(), other.getX()) && Objects.equals(this.getY(), other.getY());
     }
 
+    @Override
     public int hashCode() {
         return Objects.hash(x, y);
     }
