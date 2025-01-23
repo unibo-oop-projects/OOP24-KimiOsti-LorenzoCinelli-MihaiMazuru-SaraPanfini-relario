d65a37a7d1116f56530ae185f0fd5a7be2882ae5
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-23 13:21:02 +0100

    Fixed base url in sound and image locators

--- src/main/java/it/unibo/oop/relario/utils/impl/ImageLocators.java
+++ src/main/java/it/unibo/oop/relario/utils/impl/ImageLocators.java
@@ -9,7 +9,7 @@ import javax.swing.ImageIcon;
  * Locator class for images.
  */
 public final class ImageLocators {
-    private static final String IMAGE_BASE_URL = "resources/img/";
+    private static final String IMAGE_BASE_URL = "src/main/resources/img/";
     private static final String IMAGE_EXTENSION = ".png";
 
     private ImageLocators() { }
@@ -24,7 +24,7 @@ public final class ImageLocators {
     public static ImageIcon getFixedSizeImage(final String path, final double horizontalRatio, final double verticalRatio) {
         final String url = IMAGE_BASE_URL + path + IMAGE_EXTENSION;
         final var toolKit = Toolkit.getDefaultToolkit();
-        Image img = toolKit.getImage(url).getScaledInstance(
+        final Image img = toolKit.getImage(url).getScaledInstance(
             (int) (toolKit.getScreenSize().getWidth() * horizontalRatio),
             (int) (toolKit.getScreenSize().getHeight() * verticalRatio),
             Image.SCALE_SMOOTH);
