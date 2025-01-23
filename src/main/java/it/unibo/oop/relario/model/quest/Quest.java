fd3e34ee46cc187d88eddaa7df2ad58efbb1dd42
Sara Panfini <sara.panfini@studio.unibo.it>
2025-01-23 14:59:11 +0100

    modified quest objectives logic

--- src/main/java/it/unibo/oop/relario/model/quest/Quest.java
+++ src/main/java/it/unibo/oop/relario/model/quest/Quest.java
@@ -1,5 +1,7 @@
 package it.unibo.oop.relario.model.quest;
 
+import java.util.Optional;
+
 import it.unibo.oop.relario.model.GameEntityType;
 import it.unibo.oop.relario.model.map.Room;
 
@@ -8,12 +10,6 @@ import it.unibo.oop.relario.model.map.Room;
  */
 
 public interface Quest {
-
-    /**
-     * 
-     * @return TODO
-     */
-    String getName();
 
     /**
      * 
@@ -31,7 +27,7 @@ public interface Quest {
      * 
      * @return TODO
      */
-    GameEntityType getKeyEntityType();
+    Optional<GameEntityType> getKeyEntityType();
 
     Room getRoom();
 
