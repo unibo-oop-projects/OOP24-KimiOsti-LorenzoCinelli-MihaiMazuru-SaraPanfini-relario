ea14304bee7fe2cfc141622a550856f4b0f45523
Sara Panfini <sara.panfini@studio.unibo.it>
2025-01-23 14:57:38 +0100

    added method to verify if the npc has dropped the loot

--- src/main/java/it/unibo/oop/relario/model/entities/npc/Npc.java
+++ src/main/java/it/unibo/oop/relario/model/entities/npc/Npc.java
@@ -1,12 +1,11 @@
 package it.unibo.oop.relario.model.entities.npc;
 
-import it.unibo.oop.relario.model.GameEntityType;
 import it.unibo.oop.relario.model.entities.LivingBeing;
 
 /**
  * Interface representing a NPC in the game.
  */
-public interface Npc extends LivingBeing, GameEntityType {
+public interface Npc extends LivingBeing {
 
     /**
      * Allowes the interaction with the NPC.
