6baef083b93d31a54e2fbfafa18de4b7a4f911c4
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-01-23 16:59:31 +0100

    Removed unused import

--- src/test/java/it/unibo/oop/relario/controller/CombatControllerTest.java
+++ src/test/java/it/unibo/oop/relario/controller/CombatControllerTest.java
@@ -3,7 +3,6 @@ package it.unibo.oop.relario.controller;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import it.unibo.oop.relario.controller.api.CombatController;
