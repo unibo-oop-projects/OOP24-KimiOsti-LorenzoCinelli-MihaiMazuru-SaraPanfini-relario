fffc62c45753202b61b1dd36e6523a6b2240ccac
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-24 10:47:59 +0100

    Fixed Checkstyle violation

--- src/main/java/it/unibo/oop/relario/model/entities/npc/NpcBehavior.java
+++ src/main/java/it/unibo/oop/relario/model/entities/npc/NpcBehavior.java
@@ -1,7 +1,14 @@
 package it.unibo.oop.relario.model.entities.npc;
 
+/**
+ * Interface for the game's NPC behavior.
+ */
 public interface NpcBehavior {
 
+    /**
+     * Getter for the NPC's dialogue.
+     * @return the NPC's preset dialogue.
+     */
     String getDialogue();
-    
+
 }
