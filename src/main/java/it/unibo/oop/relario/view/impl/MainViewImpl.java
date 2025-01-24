029e349ccc7710c3fa764fbfc7ce6993cdb86210
Mihai Mazuru <mihai.mazuru@studio.unibo.it>
2025-01-24 17:36:14 +0100

    Added exit on close to the game frame

--- src/main/java/it/unibo/oop/relario/view/impl/MainViewImpl.java
+++ src/main/java/it/unibo/oop/relario/view/impl/MainViewImpl.java
@@ -78,7 +78,7 @@ public final class MainViewImpl implements MainView {
     }
 
     private void frameSetup() {
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         this.frame.setLayout(new BorderLayout());
         this.frame.setLocationByPlatform(true);
