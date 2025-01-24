5f1289d5c7e8eb46ab6e7653278a56492b0beff4
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-24 17:34:38 +0100

    Added resources urls

--- src/main/java/it/unibo/oop/relario/view/impl/CombatAnimationImpl.java
+++ src/main/java/it/unibo/oop/relario/view/impl/CombatAnimationImpl.java
@@ -7,6 +7,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.Timer;
 
+import org.apache.commons.lang3.RandomUtils;
+
 import it.unibo.oop.relario.utils.impl.Constants;
 import it.unibo.oop.relario.utils.impl.ImageLocators;
 import it.unibo.oop.relario.utils.impl.SoundLocators;
@@ -21,11 +23,22 @@ public final class CombatAnimationImpl extends JLabel implements CombatAnimation
     /** The attack is from the enemy to the player. */
     public static final boolean FROM_ENEMY_TO_PLAYER = true;
 
+    private static final long serialVersionUID = 1L;
     private static final int ANIMATION_DURATION = 850;
     private static final double RATIO = 0.6;
-    private static final String ATTACK_ANIMATION = "";
-    private static final String ATTACKED_ANIMATION = "";
-    private static final List<String> ATTACK_AUDIO = List.of();
+    private static final String ATTACK_ANIMATION = "combat/attack_effect";
+    private static final String ATTACKED_ANIMATION = "combat/attacked_effect";
+    private static final List<String> ATTACK_AUDIO = List.of(
+        "combat/Great_Heave",
+        "combat/Heavy_Slash",
+        "combat/Piercing_Thrust",
+        "combat/Quick_Swing",
+        "combat/Quick_Throw",
+        "combat/Side_Cleave",
+        "combat/Side_Swing",
+        "combat/Slash",
+        "combat/String_Jab"
+    );
 
     private final Clip clip;
     private final ImageIcon icon;
@@ -35,7 +48,7 @@ public final class CombatAnimationImpl extends JLabel implements CombatAnimation
      * @param direction is the direction of the attack.
      */
     public CombatAnimationImpl(final boolean direction) {
-        this.clip = SoundLocators.getAudio(ATTACK_AUDIO.get(0)); //TODO
+        this.clip = SoundLocators.getAudio(ATTACK_AUDIO.get(RandomUtils.nextInt(0, ATTACK_AUDIO.size())));
         this.icon = ImageLocators.getFixedSizeImage(
             direction ? ATTACKED_ANIMATION : ATTACK_ANIMATION,
             Constants.GIF_EXTENSION, RATIO, RATIO);
