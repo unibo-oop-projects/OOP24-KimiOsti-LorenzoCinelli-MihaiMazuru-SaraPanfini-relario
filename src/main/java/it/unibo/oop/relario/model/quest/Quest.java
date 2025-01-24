cf4a12a9fc685f35bb22303ec537fe50dcafc207
Kimi Osti <kimi.osti@studio.unibo.it>
2025-01-24 11:47:42 +0100

    Fixed Checkstyle violations

--- src/main/java/it/unibo/oop/relario/model/quest/Quest.java
+++ src/main/java/it/unibo/oop/relario/model/quest/Quest.java
@@ -6,29 +6,32 @@ import it.unibo.oop.relario.model.GameEntityType;
 import it.unibo.oop.relario.model.map.Room;
 
 /**
- * 
+ * Interface for a game's quest.
  */
-
 public interface Quest {
 
     /**
-     * 
-     * @return TODO
+     * Getter for the quest description.
+     * @return the description of the quest.
      */
     String getDescription();
 
     /**
-     * 
-     * @return TODO
+     * Checks if the quest is completed.
+     * @return a boolean representing if the quest is completed.
      */
     boolean isCompleted();
 
     /**
-     * 
-     * @return TODO
+     * Getter for the type of the entity concerned by the quest.
+     * @return the type of the entity concerned by the quest.
      */
     Optional<? extends GameEntityType> getKeyEntityType();
 
+    /**
+     * Getter for the room in which the quest is located.
+     * @return the room where the quest is located.
+     */
     Room getRoom();
 
 }
--- src/main/java/it/unibo/oop/relario/model/quest/QuestFactory.java
+++ src/main/java/it/unibo/oop/relario/model/quest/QuestFactory.java
@@ -6,10 +6,17 @@ import it.unibo.oop.relario.model.GameEntityType;
 import it.unibo.oop.relario.model.map.Room;
 
 /**
- * 
+ * Interface for a quest factory class.
  */
 public interface QuestFactory {
 
+    /**
+     * Creates a quest instance from a given type.
+     * @param room the room hosting the quest.
+     * @param questType the type of the quest.
+     * @param keyEntity the type of the entity related to the quest.
+     * @return an instance of a quest with all the given specifications.
+     */
     Quest createQuestByType(Room room, QuestType questType, Optional<GameEntityType> keyEntity);
 
 }
