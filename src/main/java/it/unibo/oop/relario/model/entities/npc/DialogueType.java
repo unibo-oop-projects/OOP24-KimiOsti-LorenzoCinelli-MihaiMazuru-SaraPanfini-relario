0ff4071a503c7a016a5549b5a0fe2bed26258d9d
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-24 01:02:45 +0100

    Fixed dialogue type

--- src/main/java/it/unibo/oop/relario/model/entities/npc/DialogueType.java
+++ src/main/java/it/unibo/oop/relario/model/entities/npc/DialogueType.java
@@ -1,9 +1,16 @@
 package it.unibo.oop.relario.model.entities.npc;
 
+/**
+ * Enumeration representing the dialogue types.
+ */
 public enum DialogueType {
-    
+    /** Default dialogue type. */
     DEFAULT,
+
+    /** Dialogue to retrive loot. */
     LOOT,
+
+    /** Dialogue to not retrive loot. */
     NO_LOOT;
 
 }
