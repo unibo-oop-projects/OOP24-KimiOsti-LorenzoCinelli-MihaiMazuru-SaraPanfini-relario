1d82ec5047961ae04ee698fad0300049f88b1c6b
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-24 01:32:42 +0100

    Fixed dialogues generator

--- src/main/java/it/unibo/oop/relario/model/entities/npc/DialoguesGenerator.java
+++ src/main/java/it/unibo/oop/relario/model/entities/npc/DialoguesGenerator.java
@@ -2,7 +2,7 @@ package it.unibo.oop.relario.model.entities.npc;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -13,7 +13,7 @@ import java.util.Random;
  */
 public final class DialoguesGenerator {
 
-    private final Map<DialogueType, List<String>> dialogues = new HashMap<>();
+    private final Map<DialogueType, List<String>> dialogues = new EnumMap<>(DialogueType.class);
     private final Random random;
 
     /**
@@ -21,21 +21,22 @@ public final class DialoguesGenerator {
      */
     public DialoguesGenerator() {
         this.random = new Random();
-        this.dialogues.put(DialogueType.DEFAULT, new ArrayList<>(Arrays.asList
-        ("Oggi è una bella giornata!", "Non ho niente da dire", "Sono impegnato, non mi disturbare")));
-        this.dialogues.put(DialogueType.LOOT, new ArrayList<>(Arrays.asList
-        ("Ho qualcosa che potrebbe esserti utile", "Forse questo ti potrà servire", 
+        this.dialogues.put(DialogueType.DEFAULT, new ArrayList<>(Arrays.asList(
+            "Oggi è una bella giornata!", "Non ho niente da dire", "Sono impegnato, non mi disturbare")));
+        this.dialogues.put(DialogueType.LOOT, new ArrayList<>(Arrays.asList(
+            "Ho qualcosa che potrebbe esserti utile", "Forse questo ti potrà servire", 
         "Spero che questo ti aiuti", "Ecco un regalo per te")));
-        this.dialogues.put(DialogueType.NO_LOOT, new ArrayList<>(Arrays.asList
-        ("Non ho altro da darti", "Non posso aiutarti oltre", "Mi dispiace, quello era tutto")));
+        this.dialogues.put(DialogueType.NO_LOOT, new ArrayList<>(Arrays.asList(
+            "Non ho altro da darti", "Non posso aiutarti oltre", "Mi dispiace, quello era tutto")));
     }
 
     /**
      * Retrieves a random default dialogue, for not interactive NPCs.
+     * @param dialogueType is the type of dialogue.
      * @return a random default dialogue
      */
     public String getDialogue(final DialogueType dialogueType) {
-        List<String> matchingDialogues = this.dialogues.get(dialogueType);
+        final List<String> matchingDialogues = this.dialogues.get(dialogueType);
         if (matchingDialogues.isEmpty()) {
             throw new IllegalArgumentException();
         }
