0a37b890ee16effbff7a6b3e5c439cbc8ca7cfdc
Lorenzo Cinelli <lorenzo.cinelli@studio.unibo.it>
2025-01-25 12:32:37 +0100

    Created enemy test class

---
+++ src/test/java/it/unibo/oop/relario/model/entities/enemies/EnemyTest.java
@@ -0,0 +1,34 @@
+package it.unibo.oop.relario.model.entities.enemies;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test class for the {@link Enemy} interface.
+ */
+final class EnemyTest {
+
+    /**
+     * Sets up the testing.
+     */
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    /**
+     * Tests getters methods.
+     */
+    @Test
+    void testGetters() {
+
+    }
+
+    /**
+     * Tests when the enemy is attacked.
+     */
+    @Test
+    void testAttacked() {
+
+    }
+}
