b8a1fd7f9f50c06da1469ea7471e7c1614f9f76a
Sara Panfini <sara.panfini@studio.unibo.it>
2025-01-26 21:59:23 +0100

    modified InteractionOutput implementation to use Pair

--- src/main/java/it/unibo/oop/relario/model/entities/npc/InteractionOutput.java
+++ src/main/java/it/unibo/oop/relario/model/entities/npc/InteractionOutput.java
@@ -3,13 +3,14 @@ package it.unibo.oop.relario.model.entities.npc;
 import java.util.Optional;
 
 import it.unibo.oop.relario.model.inventory.InventoryItem;
+import it.unibo.oop.relario.utils.impl.Pair;
 
 /**
  * This class represents the outcome of an interaction with NPC.
  */
 public final class InteractionOutput {
-    private final String dialogue;
-    private final Optional<InventoryItem> loot;
+
+    private final Pair<String, Optional<InventoryItem>> output;
 
     /**
      * Constructs a new InteractionOutput instance.
@@ -17,8 +18,7 @@ public final class InteractionOutput {
      * @param loot that the NPC should give to the player, if present
      */
     public InteractionOutput(final String dialogue, final Optional<InventoryItem> loot) {
-        this.dialogue = dialogue;
-        this.loot = loot;
+        this.output = new Pair<>(dialogue, loot);
     }
 
     /**
@@ -26,7 +26,7 @@ public final class InteractionOutput {
      * @return the dialogue of the NPC
      */
     public String getDialogue() {
-        return this.dialogue;
+        return this.output.getX();
     }
 
     /**
@@ -34,7 +34,7 @@ public final class InteractionOutput {
      * @return an optional containing the loot, or an empty optional if the NPC provides no item
      */
     public Optional<InventoryItem> getLoot() {
-        return this.loot;
+        return this.output.getY();
     }
 
 }
